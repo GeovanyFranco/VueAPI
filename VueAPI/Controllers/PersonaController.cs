bce60264271c5cbd4be97cbe6d6c77eaf0a6dfa6
Geokiller <63886424+GeovanyFranco@users.noreply.github.com>
2021-08-20 23:29:42 -0600

    Api creada

---
+++ VueAPI/Controllers/PersonaController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VueAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonaController : ControllerBase
+    {
+        [HttpGet]
+        public List<Persona> Get()
+        {
+
+            List<Persona> lst = null;
+
+            using (Modelos.VueContext db = new Modelos.VueContext())
+            {
+                lst = (from p in db.Personas
+                       select new Persona
+                       {
+                           Nombre = p.Nombre,
+                           Edad = p.Edad
+                       }).ToList();
+            }
+
+            return lst;
+        }
+    }
+
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+    }
+}
---
+++ VueAPI/Modelos/VueContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace VueAPI.Modelos
+{
+    public partial class VueContext : DbContext
+    {
+        public VueContext()
+        {
+        }
+
+        public VueContext(DbContextOptions<VueContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Persona> Personas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=desktop-v9egkm5;Database=Vue;Trusted_Connection=true");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Persona>(entity =>
+            {
+                entity.ToTable("Persona");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Nombre).HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
